2f44f3a57587e8e10269fe29ac0542aeb7218b42
dimas-aulia <dimaz.rizki2017@gmail.com>
2025-11-28 21:36:23 +0700

    tes commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
+// ini tes nambah teks biasa
+
 public class Main {
     public static void main(String[] args) {
-    //
+
         Scanner sc = new Scanner(System.in);
 
         ArrayList<Buku> daftarBuku = new ArrayList<>();
