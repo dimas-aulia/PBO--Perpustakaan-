c9bb65cbf1d2858c6720126640d6fec4b1243f19
dimas-aulia <dimaz.rizki2017@gmail.com>
2025-12-19 16:06:48 +0700

    tambah fungsi java anggota controler

--- src/main/java/controller/AnggotaController.java
+++ src/main/java/controller/AnggotaController.java
@@ -1,4 +1,34 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+
 public class AnggotaController {
+
+    @FXML
+    private void handleSimpan() {
+        alert("Simpan");
+    }
+
+    @FXML
+    private void handleHapus() {
+        alert("Hapus");
+    }
+
+    @FXML
+    private void handleUbah() {
+        alert("Ubah");
+    }
+
+    @FXML
+    private void handleLihat() {
+        alert("Lihat");
+    }
+
+    private void alert(String msg) {
+        Alert a = new Alert(Alert.AlertType.INFORMATION);
+        a.setHeaderText(null);
+        a.setContentText("Klik " + msg);
+        a.show();
+    }
 }
